1e9654abac7467537ac43430913a7c1dbd449305
R.miyagi <R.miyagi>
2017-12-08 00:38:32 +0900

    add xlsm file and export bat

---
+++ VBA/vbaMacroImport.bat
@@ -0,0 +1,36 @@
+@echo off
+rem 指定したフォルダ配下のvbac.wsfを実行しマクロのVBAをインポートする.
+
+rem vbac.wsfの存在するフォルダの指定
+set inputPath=ExcelVBA
+
+:main
+echo Excelからマクロのインポート処理実施しますか？
+echo * ******************************
+echo * [y] 続行する場合
+echo * [n] 終了する場合
+echo * ******************************
+set /p input="入力："
+if defined input set input=%input:"=%
+if /i "%input%" == "y" (goto start)
+if /i "%input%" == "n" (goto quit)
+rem それ以外ならコンソールをクリアし再度質問
+cls
+goto main
+
+rem 入力が"y"の場合
+:start
+cd %inputPath%
+rem ExcelよりVBAをインポート
+cscript vbac.wsf combine
+
+rem 結果表示 %~dp0はカレントパスを表示する
+echo インポート処理が完了しました.
+echo;
+tree %~dp0%inputPath% /f
+pause
+goto quit
+
+rem 入力が"n"の場合
+:quit
+exit
